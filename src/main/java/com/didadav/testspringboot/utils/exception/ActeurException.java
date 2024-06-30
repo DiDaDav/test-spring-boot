75af8c549484adb1d225dbe258b93017e32317ba
DiDaDav <david.damien.dizy@gmail.com>
2024-06-30 16:14:49 -0400

    ✨ add specific cases exceptions

---
+++ src/main/java/com/didadav/testspringboot/utils/exception/ActeurException.java
@@ -0,0 +1,7 @@
+package com.didadav.testspringboot.utils.exception;
+
+public class ActeurException extends Exception {
+    public ActeurException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/didadav/testspringboot/utils/exception/FilmNotFoundException.java
@@ -0,0 +1,7 @@
+package com.didadav.testspringboot.utils.exception;
+
+public class FilmNotFoundException extends Exception {
+    public FilmNotFoundException(String message) {
+        super(message);
+    }
+}
