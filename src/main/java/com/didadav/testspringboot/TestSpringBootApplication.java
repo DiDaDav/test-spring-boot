ac863dbfe6e947ababdc3dd2cc80ea21652aacf4
DiDaDav <david.damien.dizy@gmail.com>
2024-06-30 16:09:32 -0400

    🚚 rename src path

--- src/main/java/com/didadav/testspringboot/TestSpringBootApplication.java
+++ src/main/java/com/didadav/testspringboot/TestSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.testspringboot;
+package com.didadav.testspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
